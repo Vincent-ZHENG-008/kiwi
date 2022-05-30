ffd62b9f08b502c4d370f64ee0807a40315a6198
zhengwenhuan@gdmcmc.cn <jojo136072782>
2022-05-30 09:12:04 +0800

    feat： - messaging 模块提供读写锁支持

--- messaging/src/main/java/com/zhengwenhuan/kiwi/messaging/MessageHandler.java
+++
@@ -1,9 +0,0 @@
-package com.zhengwenhuan.kiwi.messaging;
-
-import java.util.function.Consumer;
-
-/**
- * @author zhengwenhuan@gdmcmc.cn
- */
-public interface MessageHandler extends Consumer<Message<?>> {
-}
